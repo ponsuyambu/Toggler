d455070d05d8f29597e32e0ff462dfec0ed93540
Ponsuyambu Velladurai <4482905+ponsuyambu@users.noreply.github.com>
2020-07-29 17:31:15 +0200

    feat:Splash screen (#7)
    * feat: Add theme for toggles list screen
    
    * chore: test bintray props
    
    * chore: test bintray props
    
    * chore: echo token
    
    * chore: echo token
    
    * chore: echo token
    
    * chore: echo CODECOV_TOKEN
    
    * chore: use release
    
    * chore: print
    
    * chore: fix yaml error
    
    * chore: fix yaml error
    
    * chore: fix yaml error
    
    * chore: fix env issue
    
    * chore: fix env issue
    
    * chore: revert hardcoded version
    
    * chore: upload snapshot builds to github
    
    * chore: upload snapshot builds to github
    
    * chore: upload snapshot builds to github
    
    * chore: remove each push check

--- app/src/main/java/in/ponshere/featuretoggler/MainActivity.kt
+++ app/src/main/java/in/ponshere/featuretoggler/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.FirebaseApp
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        FirebaseApp.initializeApp(this)
-        FirebaseRemoteConfig.getInstance().fetch(60).addOnCompleteListener {
-            if (it.isSuccessful) {
-                FirebaseRemoteConfig.getInstance().activate()
-                runOnUiThread {
-                    val text =
-                        "feature3Enabled: ${Toggler.get<AppTogglesV2>().getFeature3Option()}"
-                    btnLaunch.text = text
 
-                    Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                runOnUiThread {
-                    Toast.makeText(this, "Failed: ${it.exception}", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
         Log.d(
             "Toggle",
             "toggles.isFeature2Enabled() " + Toggler.get<AppTogglesV2>().isFeature2Enabled()
